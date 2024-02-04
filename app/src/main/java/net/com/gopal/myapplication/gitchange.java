92948064bf1f3978d8fb978cdc3e779e7c5560c4
safal chettri <safalmahatara8848@gmail.com>
2024-02-04 11:26:47 +0545

    Verison2

--- app/src/main/java/net/com/gopal/myapplication/gitchange.java
+++ app/src/main/java/net/com/gopal/myapplication/gitchange.java
@@ -2,7 +2,7 @@ package net.com.gopal.myapplication;
 
 public class gitchange {
         public static void main(String[] args) {
-                String string = "this is String";
+                String string = "this is integer";
                 System.out.println(string);
         }
 
