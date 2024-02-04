87606946a8e52d6e5fed90db34805d31fde6f5b3
safal chettri <safalmahatara8848@gmail.com>
2024-02-04 11:16:38 +0545

    Version 1

---
+++ app/src/main/java/net/com/gopal/myapplication/gitchange.java
@@ -0,0 +1,9 @@
+package net.com.gopal.myapplication;
+
+public class gitchange {
+        public static void main(String[] args) {
+                String string = "this is String";
+                System.out.println(string);
+        }
+
+}
