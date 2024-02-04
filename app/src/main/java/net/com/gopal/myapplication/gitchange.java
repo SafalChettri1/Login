4ae55707caf468b72a71248cb2c14e4c00998dc0
safal chettri <safalmahatara8848@gmail.com>
2024-02-04 13:53:21 +0545

    Verion6

--- app/src/main/java/net/com/gopal/myapplication/gitchange.java
+++ app/src/main/java/net/com/gopal/myapplication/gitchange.java
@@ -2,7 +2,7 @@ package net.com.gopal.myapplication;
 
 public class gitchange {
         public static void main(String[] args) {
-                String string = "this is version5";
+                String string = "this is version6";
 
                 System.out.println(string);
         }
